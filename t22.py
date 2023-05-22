9508c34a651355def40a3380e01855a4f4f82a1a
bookyongwhy bookyongwhy <1029667436@qq.com>
2023-05-22 12:39:31 +0000

    t28

---
+++ t22.py
@@ -0,0 +1,7 @@
+s = "ajldjlajfdljfddd"
+s=set(s)
+s=list(s)
+s.sort(reverse=False)
+for k in s:
+    print(k,end='')
+print()
---
+++ t28.py
@@ -0,0 +1,3 @@
+a=range(1,10)
+b=[i for i in a if i%2!=0]
+print(b)       
